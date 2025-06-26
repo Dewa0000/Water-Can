9219b72868248986eb9541867ceb083b4a5a2d6c
Dewa0000 <dewashsishsahu8888@gmail.com>
2025-06-26 12:11:43 +0530

    10th

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 import connectDB from './Config/db.js';
 import productRoutes from './Routes/productRoutes.js';
 import dotenv from 'dotenv';
