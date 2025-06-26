41d038b724067742886dafdde07e3f920d8c2386
Dewa0000 <dewashsishsahu8888@gmail.com>
2025-06-26 17:17:21 +0530

    15th

--- Frontend/water-can/src/Pages/Home.jsx
+++ Frontend/water-can/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from "../Components/Hero";
 function Home() {
   return <div className="text-center p-10">
       <Hero/>
-      <div className="grid">
+      <div className="grid grid-cols">
         <div>hi</div>
         <div>hi</div>
         <div>hi</div>
