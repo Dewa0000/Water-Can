989541d710b8daf19860ed69f266a0c445c07758
Dewa0000 <dewashsishsahu8888@gmail.com>
2025-06-26 16:44:55 +0530

    13th

--- Backend/Routes/productRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllProducts } = require('../Controllers/productController');
-
-router.get('/', getAllProducts);
-
-module.exports = router;
