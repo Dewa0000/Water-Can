bfbe1f510e45ffcc4d689a88f6f7ebac10ed72d1
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-07-16 20:32:52 +0530

    44th

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -5,29 +5,43 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 const createToken = (_id) => {
-      return jwt.sign({_id},process.env.SECRET,{expiresIn:"3d"})
+      return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" })
 }
 
-router.post("/", async (req,res) => {
-      const {fullName,email,phoneNumber,password} = req.body;
-       try{
-            const user = await User.signup(fullName,email,phoneNumber,password);
+router.post("/", async (req, res) => {
+      const { fullName, email, phoneNumber, password } = req.body;
+      try {
+            const user = await User.signup(fullName, email, phoneNumber, password);
             const token = createToken(user._id);
-            res.status(200).json({email,token})
-       }catch(err){
-            res.status(400).json({error: err.message})
-       }
+            res.status(200).json({
+                  user: {
+                        _id: user._id,
+                        fullName: user.fullName,
+                        email: user.email,
+                        phoneNumber: user.phoneNumber
+                  }, token
+            })
+      } catch (err) {
+            res.status(400).json({ error: err.message })
+      }
 })
 
-router.post("/login", async (req,res) => {
-      const {fullName,email,phoneNumber,password} = req.body;
+router.post("/login", async (req, res) => {
+      const { fullName, email, phoneNumber, password } = req.body;
 
-      try{
-          const user = await User.login(email,password);
-          const token = createToken(user._id);
-          res.status(200).json({email,token});
-      }catch(err){
-          res.status(400).json({error: err.message})
+      try {
+            const user = await User.login(email, password);
+            const token = createToken(user._id);
+            res.status(200).json({
+                  user: {
+                        _id: user._id,
+                        fullName: user.fullName,
+                        email: user.email,
+                        phoneNumber: user.phoneNumber
+                  }, token
+            });
+      } catch (err) {
+            res.status(400).json({ error: err.message })
       }
 })
 
