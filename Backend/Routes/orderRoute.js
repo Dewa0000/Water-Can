2347dc9dd1ac411858edbd8eaa9b397a8c1f79a7
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-07-24 19:32:05 +0530

    61st

--- Backend/Routes/orderRoute.js
+++ Backend/Routes/orderRoute.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const Order = require("../Models/Order");
 const authMiddleware = require("../Middleware/requireAuth");
 
-
-
-router.get("/", authMiddleware, async (req, res) => {
+router.get("/my-orders", authMiddleware, async (req,res) => {
   try {
-    const orders = await Order.find({ userId: req.userId }).sort({ createdAt: -1 });
-    res.status(200).json(orders);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const orders = await Order.find({userId : req.userId}).sort({createdAt : -1});
+    res.json(orders);
+  }catch(err){
+    res.status(400).json({err: "Failed to fetch Orders"})
   }
-});
+})
+
+
 
 router.post("/",authMiddleware, async (req, res) => {
   try {
