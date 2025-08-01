527c61e48e9312785a07d2a190d6024b235801a6
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-08-01 11:18:15 +0530

    81st

--- Frontend/water-can/src/Hooks/useFetchProducts.jsx
+++ Frontend/water-can/src/Hooks/useFetchProducts.jsx
@@ -11,8 +11,8 @@ const FetchProducts = () => {
                try{
             const res = await fetch(`${backendUrl}/products`)
             const data = await res.json();
-            const spliced = data.splice(2, -3) // Get elements from index 2 to 3 (exclusive)
-            setProducts(spliced); 
+            const sliced = data.slice(2, -3) // Get elements from index 2 to 3 (exclusive)
+            setProducts(sliced); 
         }catch(err){
             console.log(err.message)
         }
